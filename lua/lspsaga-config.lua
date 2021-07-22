deec9c7805ebf60abdec9831437626aa0f46f252
Omar Almalol <omar.alma3lol@gmail.com>
2021-07-22 05:46:08 +0200

    feat(lua/lsp-saga): adds keymaps and border_style keys to setup

--- lua/lspsaga-config.lua
+++ lua/lspsaga-config.lua
@@ -1,3 +1,9 @@
 local saga = require 'lspsaga'
 
-saga.init_lsp_saga()
+saga.init_lsp_saga {
+	code_action_keys = { quit = '<ESC>',exec = '<CR>' },
+	finder_action_keys = {
+		open = '<CR>', vsplit = 'v',split = 's',quit = '<ESC>',scroll_down = '<C-f>', scroll_up = '<C-b>'
+	},
+	border_style = "round"
+}
