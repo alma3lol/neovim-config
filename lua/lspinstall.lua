ddfc7663833dff7ee601f8580ca371fef9c72378
Omar Almalol <omar.alma3lol@gmail.com>
2021-08-04 15:12:32 +0200

    chore(lua/lspinstall): removes comments

--- lua/lspinstall.lua
+++ lua/lspinstall.lua
@@ -8,8 +8,7 @@ end
 
 setup_servers()
 
--- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
 require'lspinstall'.post_install_hook = function ()
-	setup_servers() -- reload installed servers
-	vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
+	setup_servers()
+	vim.cmd("bufdo e")
 end
