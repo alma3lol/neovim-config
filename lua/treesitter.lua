58684c1d14de360d1030e4a1503f578f732cb74f
Omar Almalol <omar.alma3lol@gmail.com>
2021-11-04 20:05:55 -0400

    refactor(lua): refactors treesitter config

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,10 +1,32 @@
 require('nvim-treesitter.configs').setup {
-	ensure_installed = {'javascript', 'tsx', 'typescript', 'regex', 'python', 'lua', 'jsonc', 'json', 'jsdoc', 'html', 'css', 'dockerfile'},
-	highlight = { enable = true },
+	ensure_installed = {
+		'javascript',
+		'tsx',
+		'typescript',
+		'regex',
+		'python',
+		'lua',
+		'jsonc',
+		'json',
+		'jsdoc',
+		'html',
+		'css',
+		'dockerfile'
+	},
+	highlight = {
+		enable = true,
+		use_languagetree = true
+	},
 	incremental_selection = {enable = true},
 	textobjects = {enable = true},
-	indent = {enable = true},
-	rainbow = {enable = true, extended_mode = true}
+	indent = {enable = false},
+	rainbow = {enable = true, extended_mode = true},
+	context_commentstring = {enable = true},
+	query_linter = {
+		enable = true,
+		use_virtual_text = true,
+		lint_events = {"BufWrite", "CursorHold"}
+	}
 }
 
 
