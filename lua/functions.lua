19016b2475967b39320b89c2331e0751c955bd7c
Omar Almalol <omar.alma3lol@gmail.com>
2021-12-08 20:33:09 +0200

    chore: comments out debugging information

--- lua/functions.lua
+++ lua/functions.lua
@@ -4,6 +4,9 @@ local lspSaga = require('lspsaga.hover')
 
 function _G.HoverInfo()
 	local debugSession = dap.session()
+	-- local status = debugSession.status();
+	-- print(debugSession)
+	-- print(status)
 	if debugSession == nil or debugSession.stopped_thread_id == nil then
 		lspSaga.render_hover_doc()
 	else
